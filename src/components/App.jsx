8144c533c5e53d92917ecd667d06836700e417ea
Tymur Qayoom <tymursheikh4576@gmail.com>
2023-10-06 20:38:13 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,39 @@ import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import PixabayAPI from '../api/pixabayAPI'
 
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    searchData: null,
   };
 
-  handlerFormSubmit = searchQuery => {
+  apiService = new PixabayAPI();  // Создаем экземпляр PixabayAPI
+
+  handleFormSubmit = searchQuery => {
     this.setState({ searchQuery });
+    this.apiService.query = searchQuery;
+    this.fetchData();
+  };
+
+  fetchData = async () => {
+    try {
+      const data = await this.apiService.fetchSearch();
+      this.setState({ searchData: data });
+    } catch (error) {
+      console.error('Ошибка загрузки данных:', error);
+    }
   };
 
   render() {
-    const { searchQuery } = this.state;
+    const { searchQuery, searchData } = this.state;
     return (
       <>
-        <SearchBar onSubmit={this.handlerFormSubmit} />
-
-        <ImageGallery searchQuery={searchQuery} />
+        <SearchBar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchQuery={searchQuery} searchData={searchData} />
         <ToastContainer />
       </>
     );
